547680f974098bb9bef62634a8fdb792ea4782dd
portal-ui <109450499+portal-ui@users.noreply.github.com>
2025-02-19 16:05:30 +0700

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,20 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     }
   });
 });
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  autoplay: {
+    delay: 5000,
+  },
+
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+})
