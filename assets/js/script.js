0c101d096c76774103fec986eb6e022b1a405d53
portal-ui <109450499+portal-ui@users.noreply.github.com>
2025-02-22 00:56:21 +0700

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,19 +13,40 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   });
 });
 
-const swiper = new Swiper('.swiper', {
+document.addEventListener("DOMContentLoaded", function () {
+  const cards = document.querySelectorAll(".card");
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry, index) => {
+      if (entry.isIntersecting) {
+        setTimeout(() => {
+          entry.target.classList.add("animate__fadeInUp");
+          entry.target.style.opacity = 1;
+          entry.target.style.transform = "translateY(0)";
+        }, index * 200); // Delay bertahap per box (200ms per item)
+      }
+    });
+  }, { threshold: 0.3 });
+
+  cards.forEach((card) => {
+    observer.observe(card);
+  });
+});
+
+const swiper = new Swiper(".swiper", {
   loop: true,
-
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
-
   autoplay: {
     delay: 5000,
   },
-
   scrollbar: {
-    el: '.swiper-scrollbar',
+    el: ".swiper-scrollbar",
   },
-})
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+});
